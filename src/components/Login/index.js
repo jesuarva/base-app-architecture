2051c07d5487470285b7877ec5d5d43d74af9651
jesuarva <jesuarva.developer@gmail.com>
2020-03-22 22:02:06 +0000

    Implement Login-form

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -1,21 +1,54 @@
-import React, { Fragment, useContext, useEffect } from 'react'
+import React, { useReducer, useContext, useEffect, useCallback } from 'react'
 import { useHistory } from 'react-router-dom'
 import StateContext, { STATES } from 'state/index'
+import { loginReducer, initialState, ACTIONS } from './loginState'
 import styles from './styles.module.scss'
 
 export default function Login() {
   const {
     state: { currentState },
-    dispatch,
+    dispatch: dispatchToGlobalState,
   } = useContext(StateContext)
+  const [state, dispatchToLogin] = useReducer(loginReducer, initialState)
   const history = useHistory()
+
+  const handleChange = useCallback(({ target }) => {
+    dispatchToLogin({ type: ACTIONS.updateField, field: target.id, value: target.value })
+  }, [])
+  const handleSubmit = useCallback(event => {
+    event.preventDefault()
+  }, [])
+
   useEffect(() => {
-    setTimeout(dispatch, 2000, { type: 'logUser' })
-  }, [dispatch])
+    setTimeout(dispatchToGlobalState, 2000, { type: 'logUser' })
+  }, [dispatchToGlobalState])
   useEffect(() => {
     if (currentState === STATES.loggedIn) history.push('/')
   }, [currentState, history])
-  console.log('Login 2', { state: currentState })
 
-  return <Fragment>{`State: ${currentState}`}</Fragment>
+  console.log('Login 2', { state: currentState, form: state.form })
+
+  return (
+    <div className={`component-container ${styles.wrapper}`}>
+      <form onSubmit={handleSubmit} onChange={handleChange}>
+        <div className={styles.field}>
+          <label htmlFor="username">
+            <span className={styles['field-label']}>Username</span>
+            <span className={styles['field-hint']}> The username you chose at signup. </span>
+          </label>
+          <input type="text" id="username" name="password" />{' '}
+        </div>
+        <div className={styles.field}>
+          <label htmlFor="password">
+            <span className={styles['field-label']}>Password</span>
+            <span className={styles['field-hint']}>Must contain _detail_password_requirements_.</span>
+          </label>
+          <input type="password" id="password" name="password" />
+        </div>
+        <div className={styles.submit}>
+          <input type="submit" id="submit" value="Log In" />
+        </div>
+      </form>
+    </div>
+  )
 }

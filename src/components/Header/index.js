b1609a58c8ec34aaf657da06db1fe1e94deb4792
jesuarva <jesuarva.developer@gmail.com>
2020-03-22 15:35:54 +0000

    Add `log out` functionality

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,16 +1,24 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { Link } from 'react-router-dom'
+import StateContext, { STATES } from 'state/index'
+import { Button } from 'reakit/Button'
 import styles from './styles.module.scss'
 
 function Header(props) {
+  const {
+    state: { currentState },
+    dispatch,
+  } = useContext(StateContext)
   return (
     <header className={styles.header}>
-      <Link className={styles.link} to="home">
+      <Link className={styles.link} to="/">
         Home
       </Link>
-      <Link className={styles.link} to="login">
-        Login
-      </Link>
+      {currentState === STATES.loggedIn && (
+        <Button className={styles['logout-button']} onClick={() => dispatch({ type: 'logoutUser' })}>
+          Logout
+        </Button>
+      )}
     </header>
   )
 }

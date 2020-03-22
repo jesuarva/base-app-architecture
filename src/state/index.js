60d2c16b4608ea5447a87b5ae43b6d022abf5d38
jesuarva <jesuarva.developer@gmail.com>
2020-03-22 15:31:05 +0000

    Define `loggedOutReducer` and `loggedInReducer` reducers

--- src/state/index.js
+++ src/state/index.js
@@ -1,9 +1,16 @@
 import { createContext } from 'react'
 import produce from 'immer'
 
+export default createContext()
+
+export const STATES = {
+  loggedOut: 'LOGGED_OUT',
+  loggedIn: 'LOGGED_IN',
+}
+
 export const initialState = {
   previousState: '',
-  currentState: 'loggedOut',
+  currentState: STATES.loggedOut,
   auth: {
     user: '',
     password: '',
@@ -11,16 +18,38 @@ export const initialState = {
 }
 
 export function reducer(state = initialState, action) {
+  console.log('Reducer:', action)
+
   switch (state.currentState) {
-    case 'loggedOut': {
-      return produce(state, draftState => {})
+    case STATES.loggedOut: {
+      return loggedOutReducer(state, action)
     }
-    case 'loggedIn': {
-      return produce(state, draftState => {})
+    case STATES.loggedIn: {
+      return loggedInReducer(state, action)
     }
     default:
       return state
   }
 }
 
-export default createContext()
+function loggedOutReducer(state, action) {
+  switch (action.type) {
+    case 'logUser':
+      return produce(state, draftState => {
+        draftState.currentState = STATES.loggedIn
+      })
+    default:
+      return state
+  }
+}
+
+function loggedInReducer(state, action) {
+  switch (action.type) {
+    case 'logoutUser':
+      return produce(state, draftState => {
+        draftState.currentState = STATES.loggedOut
+      })
+    default:
+      return state
+  }
+}

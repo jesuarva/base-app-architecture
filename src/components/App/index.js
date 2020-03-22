3cc878ecfae2a0ecf731f5c7bedc0e85b2b1c2db
jesuarva <jesuarva.developer@gmail.com>
2020-03-22 00:27:46 +0000

    Define starting App skeleton

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,19 +1,21 @@
 import React from 'react'
-import logo from '../../assets/logo.svg'
-import './App.scss'
+import Login from '../Login'
+import styles from './App.module.scss'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
+    <div className={styles.app}>
+      <header className={styles.header}>
+        <a href="#!">Nav link</a>
+        <a href="#!">Nav link</a>
       </header>
+      <main className={styles.main}>
+        <Login />
+      </main>
+      <footer className={styles.footer}>
+        <a href="#!">Legal</a>
+        <a href="#!">Contact</a>
+      </footer>
     </div>
   )
 }
